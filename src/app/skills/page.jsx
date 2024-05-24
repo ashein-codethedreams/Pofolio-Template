49034fdbcf895dffa946916308813709f56fbba1
ashein-codethedreams <heroglasses155@gmail.com>
2024-05-24 10:48:17 +0630

    init pages

---
+++ src/app/skills/page.jsx
@@ -0,0 +1,7 @@
+export default function SkillsPage() {
+  return (
+    <div className="min-h-screen bg-white">
+      <p>Skills</p>
+    </div>
+  );
+}
---
+++ src/app/work/page.jsx
@@ -0,0 +1,7 @@
+export default function WorkPage() {
+  return (
+    <div className="min-h-screen bg-white">
+      <p>Work</p>
+    </div>
+  );
+}
